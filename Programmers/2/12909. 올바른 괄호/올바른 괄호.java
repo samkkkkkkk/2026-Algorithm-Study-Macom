51761289c226c88c79b5074d568c611c04c90c94
SamK5678 <142370028+samkkkkkkk@users.noreply.github.com>
2026-08-10 20:52:11 +0900

    [level 2] Title: 올바른 괄호, Time: 7.27 ms, Memory: 58.3 MB -BaekjoonHub

---
+++ Programmers/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,20 @@
+
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+class Solution {
+boolean solution(String s) {
+        Deque<Character> deque = new ArrayDeque<>();
+
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                deque.push(c);
+            } else { 
+                if (deque.isEmpty()) return false;
+                deque.pop();
+            }
+        }
+
+        return deque.isEmpty();
+    }
+}
