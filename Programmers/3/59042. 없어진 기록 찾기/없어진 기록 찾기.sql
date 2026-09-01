64750c760820ff9ad1d4cefa29f3875c1034a5f3
SamK5678 <142370028+samkkkkkkk@users.noreply.github.com>
2026-09-01 21:22:57 +0900

    [level 3] Title: 없어진 기록 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ Programmers/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
@@ -0,0 +1,9 @@
+-- 코드를 입력하세요
+SELECT
+    o.animal_id,
+    o.name
+FROM animal_ins i
+RIGHT JOIN animal_outs o
+ON i.animal_id = o.animal_id
+WHERE i.intake_condition IS NULL
+ORDER BY o.animal_id ASC;
